2574a0d2f65ebd6c7a715658ec25d5dd4fdff637
DinisBSilva <up202306207@up.pt>
2025-04-22 22:04:46 +0100

    Hot Fix

--- templates/common/header.php
+++ templates/common/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>Sixerr</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="icon" type="image/png" href="/images/sixerr_logo_tab.png">
+    <link rel="icon" type="image/png" href="/images/sixerr_favicon.png">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/home_page.css">
