64367d6962b785742804e72d35b8161dabc378b5
DinisBSilva <up202306207@up.pt>
2025-04-30 20:53:50 +0100

    Now Header and Footer behave like templates

--- templates/common/header.tpl.php
+++ templates/common/header.tpl.php
@@ -1,40 +1,52 @@
-<?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sixerr</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="icon" type="image/png" href="/images/sixerr_favicon.png">
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/home_page.css">
-    <link rel="stylesheet" href="/css/typing_effect.css">
-    <link rel="stylesheet" href="/css/category_menu.css">
-    <link rel="stylesheet" href="/css/service_cards_slider.css">
-    <link rel="stylesheet" href="/css/service_page.css">
-    <link rel="stylesheet" href="/css/authentication.css">
-    <link rel="stylesheet" href="/css/color_scheme.css">
-    <link rel="stylesheet" href="/css/toast_message.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
-</head>
-<body>
-<header>
-    <div class="logo">
-    <a href="/index.php">
-        <img src="/images/sixerr_logo.png" alt="Sixerr logo">
-    </a>
-</div>
+<?php 
+declare(strict_types=1);
 
-    <nav>
-        <?php if (!isset($_SESSION['user'])): ?>
-            <a href="/pages/login.php">Login</a>
-            <a href="/pages/register.php">Register</a>
-        <?php else: ?>
-            <a href="dashboard.php">Dashboard</a>
-            <a href="/actions/action_logout.php">Logout</a>
-        <?php endif; ?>
-    </nav>
-</header>
-<main>
+require_once(__DIR__ . '/../includes/session.php');
+?>
+
+<?php function drawHeader() { ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sixerr</title>
+        <link rel="icon" type="image/png" href="/images/sixerr_favicon.png">
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/header.css">
+        <link rel="stylesheet" href="/css/footer.css">
+        <link rel="stylesheet" href="/css/home_page.css">
+        <link rel="stylesheet" href="/css/typing_effect.css">
+        <link rel="stylesheet" href="/css/category_menu.css">
+        <link rel="stylesheet" href="/css/service_cards_slider.css">
+        <link rel="stylesheet" href="/css/service_page.css">
+        <link rel="stylesheet" href="/css/authentication.css">
+        <link rel="stylesheet" href="/css/color_scheme.css">
+        <link rel="stylesheet" href="/css/toast_message.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap">
+    </head>
+    <body>
+    <header>
+        <div class="logo">
+        <a href="/index.php">
+            <img src="/images/sixerr_logo.png" alt="Sixerr logo">
+        </a>
+    </div>
+
+        <nav>
+            <?php if (Session::getInstance()->getUser()) drawLogoutOptions(); else drawLoginOptions(); ?>       
+        </nav>
+    </header>
+    <main>
+<?php } ?>
+
+<?php function drawLoginOptions() { ?>
+    <a href="/pages/login.php">Login</a>
+    <a href="/pages/register.php">Register</a>
+<?php } ?>
+
+<?php function drawLogoutOptions() { ?>
+    <a href="dashboard.php">Dashboard</a>
+    <a href="/actions/action_logout.php">Logout</a>
+<?php } ?>        
