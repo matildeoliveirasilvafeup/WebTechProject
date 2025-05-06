c205dc8555ef0b9e3e8ae621e23a95693098651f
DinisBSilva <up202306207@up.pt>
2025-05-06 19:05:51 +0100

    Search Bar Implemented on Seach Page

--- pages/search.php
+++ pages/search.php
@@ -1,12 +1,14 @@
 <?php
     require_once(__DIR__ .  '/../templates/common/header.tpl.php');
     require_once(__DIR__ .  '/../templates/category.tpl.php');
+    require_once(__DIR__ .  '/../templates/search.tpl.php');
+    require_once(__DIR__ .  '/../templates/common/footer.tpl.php');
 
     $categories = Category::getAllWithSubcategories();
+    $searchQuery = $_GET['q'] ?? '';
 
     drawHeader();
-    drawCategoryMenu($categories); 
-
-
+    drawCategoryMenu($categories);
+    echo drawSearchForm('search.php', 'Search services...', false, 'alt-style', $searchQuery);
     drawFooter();
 ?>
