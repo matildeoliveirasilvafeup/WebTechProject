c5089b50da8581d6f4963b582eae3eafeb876213
matildeoliveirasilvafeup <up202305722@up.pt>
2025-05-23 12:16:52 +0100

    in progress media queries, typing-effect done

--- templates/common/utils.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php function renderUserLink($username, $name) {
-    $username = htmlspecialchars($username);
-    $name = htmlspecialchars($name);
-    return "<a href='/pages/profile.php?user=$username'>$name</a>";
-}
-?>
