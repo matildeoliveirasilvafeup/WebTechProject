695c9b6555d5e04440d4c790485267b460de0d23
matildeoliveirasilvafeup <up202305722@up.pt>
2025-05-25 22:44:49 +0100

    database

--- database/sixerr.sql
+++ database/sixerr.sql
@@ -158,18 +158,7 @@ CREATE TABLE custom_offers (
     FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-INSERT INTO users (name, username, email, password_hash, role) VALUES
-('João Silva', 'joaosilva', 'joao@example.com', 'hash1', 'user'),
-('Maria Costa', 'mariac', 'maria@example.com', 'hash2', 'admin'),
-('Ricardo Melo', 'ricardomelo', 'ricardo@example.com', 'hash3', 'user'),
-('Ana Sousa', 'anasousa', 'ana@example.com', 'hash4', 'user');
 
-
-INSERT INTO profiles (user_id, bio, profile_picture, is_freelancer, is_client) VALUES
-(1, 'Designer gráfico com 5 anos de experiência.', 'https://picsum.photos/200?1', 1, 1),
-(2, 'Gestora de plataforma.', 'https://picsum.photos/200?2', 0, 1),
-(3, 'Desenvolvedor web especializado em front-end.', 'https://picsum.photos/200?3', 1, 1),
-(4, 'Empresária à procura de serviços criativos.', 'https://picsum.photos/200?4', 0, 1);
 
 
 INSERT INTO categories (name, icon) VALUES
@@ -247,57 +236,3 @@ INSERT INTO subcategories (category_id, name) VALUES
 (9, 'Marketing Strategy'),
 (9, 'Tech Consulting'),
 (9, 'HR & Recruiting');
-
-INSERT INTO services (freelancer_id, title, description, category_id, subcategory_id, price, delivery_time, number_of_revisions, language, average_rating, total_reviews) VALUES
-(1, 'Unique Logo Design', 'Custom logo tailored to your brand identity.', 1, 1, 75.00, 3, 2, 'Italian/French', 4.75, 4),
-(3, 'Professional Landing Page', 'Modern, responsive landing page design.', 2, 8, 150.00, 5, 3, 'English', 4.00, 2),
-(1, 'Business Card Design', 'Stylish and modern business cards for your brand.', 1, 2, 40.00, 2, 1, 'French', 5.00, 2),
-(1, 'Company Website in HTML/CSS', 'Responsive website with up to 5 pages.', 2, 4, 200.00, 7, 4, 'Spanish/English', 3.00, 1),
-(1, 'Social Media Template Pack', 'Set of 10 customizable Instagram templates.', 1, 5, 60.00, 3, 2, 'Portuguese', 4.00, 1),
-(3, 'Landing Page with Contact Form', 'Lead-optimized landing page with working contact form.', 2, 9, 130.00, 4, 2, 'English', 5.00, 1),
-(1, 'Brand Identity Manual', 'Complete visual identity guide with colors, typography, and usage rules.', 1, 6, 120.00, 6, 1, 'Portuguese', 5.00, 1),
-(1, 'Brand Identity Manual1', 'Complete visual identity guide with colors, typography, and usage rules.', 1, 6, 120.00, 6, 1, 'Portuguese', 0.00, 0),
-(1, 'Brand Identity Manual2', 'Complete visual identity guide with colors, typography, and usage rules.', 1, 6, 120.00, 6, 1, 'Portuguese', 0.00, 0);
-
-
-INSERT INTO service_images (service_id, media_url) VALUES
-(1, 'https://picsum.photos/300?logo1'),
-(2, 'https://picsum.photos/300?landing1'),
-(3, 'https://picsum.photos/300?card1'),
-(4, 'https://picsum.photos/300?website1'),
-(5, 'https://picsum.photos/300?template1'),
-(6, 'https://picsum.photos/300?landing2'),
-(7, 'https://picsum.photos/300?identity1');
-
-INSERT INTO reviews (service_id, client_id, rating, comment) VALUES
-(1, 4, 5, 'Excellent service, highly recommended!'),
-(1, 3, 5, 'Perfect service, amazing work!'),
-(1, 1, 4, 'Loved it!'),
-(2, 2, 4, 'Very professional, but could be a bit faster.'),
-(3, 4, 5, 'Loved the business card design, it’s exactly what I needed.'),
-(4, 2, 3, 'Good work, but took longer than expected.'),
-(5, 4, 4, 'Nice templates for Instagram, helped boost my brand.'),
-(6, 2, 5, 'Landing page looks great and works perfectly.'),
-(7, 4, 5, 'The brand guide was super useful, great attention to detail.'),
-(2, 4, 4, 'Clean layout, fits well on mobile devices.'),
-(3, 2, 5, 'Super fast delivery and great communication.'),
-(1, 2, 5, 'Amazing logo, captured my vision perfectly.');
-
-INSERT INTO conversations (id, service_id, user1_id, user2_id) VALUES
-('5_6', 1, 5, 6),
-('5_7', 2, 5, 7);
-
-INSERT INTO messages (conversation_id, service_id, sender_id, receiver_id, message) VALUES
-('5_6', 1, 6, 5, 'Olá, estou interessada no teu serviço de design.'),
-('5_6', 1, 5, 6, 'Olá Ana! Claro, como posso ajudar?');
--- ('5_6', 2, 5, 6, 'Preciso de ajuda com o site da empresa.'),
--- ('5_6', 2, 5, 6, 'Claro, posso marcar uma call para hoje.');
-
-INSERT INTO hirings (service_id, client_id, owner_id) VALUES
-(1, 5, 6),
-(1, 7, 6),
-(3, 5, 6),
-(6, 5, 6),
-(5, 6, 5),
-(4, 6, 5),
-(2, 6, 5);
